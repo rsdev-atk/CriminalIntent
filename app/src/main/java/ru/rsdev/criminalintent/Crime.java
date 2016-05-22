793409ed61af692e3da5848d4253f3f4e844aa34
Дом <rsdev.info@gmail.com>
2016-05-22 23:39:25 +0700

    Глава 8, 22/05

--- app/src/main/java/ru/rsdev/criminalintent/Crime.java
+++ app/src/main/java/ru/rsdev/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package ru.rsdev.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,20 +9,39 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime(){
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
-    public UUID getmId() {
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+
+    public UUID getId() {
         return mId;
     }
 
-    public String getmTitle() {
+    public String getTitle() {
         return mTitle;
     }
 
-    public void setmTitle(String mTitle) {
+    public void setTitle(String mTitle) {
         this.mTitle = mTitle;
     }
 }
--- app/src/main/java/ru/rsdev/criminalintent/CrimeFragment.java
+++ app/src/main/java/ru/rsdev/criminalintent/CrimeFragment.java
@@ -7,12 +7,17 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    public CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle saveInstanceState){
@@ -24,6 +29,10 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_crime,container,false);
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
 
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
@@ -33,7 +42,7 @@ public class CrimeFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                mCrime.setmTitle(charSequence.toString());
+                mCrime.setTitle(charSequence.toString());
             }
 
             @Override
@@ -41,6 +50,15 @@ public class CrimeFragment extends Fragment {
 
             }
         });
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mCrime.setSolved(mSolvedCheckBox.isChecked());
+            }
+        });
+
 
         return v;
     }
